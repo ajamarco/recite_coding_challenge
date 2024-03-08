c198aee8d9055065f99c58cc6a08b98bc3f1e184
Alex Jamarco <a.jamarco@gmail.com>
2024-03-08 08:17:49 +0000

    Refactor Sidebar component to dynamically generate navigation links

--- app/components/Sidebar.tsx
+++ app/components/Sidebar.tsx
@@ -1,9 +1,24 @@
+"use client";
+
 import Link from "next/link";
 import React from "react";
+import { usePathname } from "next/navigation";
 
 const Sidebar = () => {
+  //get the current pathname
+  const pathname = usePathname();
+
+  //save the classnames for the active and inactive links
+  const activeLink =
+    "flex items-center active-nav-link text-white py-4 pl-6 nav-item";
+  const inactiveLink =
+    "flex items-center text-white opacity-75 hover:opacity-100 py-4 pl-6 nav-item";
+
+  //manually saving the categories' names - this can come from an API
+  const categories = ["Home Page", "Missions", "Ships", "Rockets"];
+
   return (
-    <aside className="relative bg-sidebar h-screen w-64 hidden sm:block shadow-xl">
+    <aside className="relative bg-sidebar h-screen w-64 sm:block shadow-xl">
       <div className="p-6">
         <Link
           href="/"
@@ -13,30 +28,21 @@ const Sidebar = () => {
         </Link>
       </div>
       <nav className="text-white text-base font-semibold pt-3">
-        <Link
-          href="/"
-          className="flex items-center active-nav-link text-white py-4 pl-6 nav-item"
-        >
-          Home Page
-        </Link>
-        <Link
-          href="/missions"
-          className="flex items-center text-white opacity-75 hover:opacity-100 py-4 pl-6 nav-item"
-        >
-          Missions
-        </Link>
-        <Link
-          href="/ships"
-          className="flex items-center text-white opacity-75 hover:opacity-100 py-4 pl-6 nav-item"
-        >
-          Ships
-        </Link>
-        <Link
-          href="/rockets"
-          className="flex items-center text-white opacity-75 hover:opacity-100 py-4 pl-6 nav-item"
-        >
-          Rockets
-        </Link>
+        {/* map through the categories object and display the links */}
+        {categories.map((category) => (
+          <Link
+            href={category === "Home Page" ? "/" : `/${category.toLowerCase()}`}
+            className={
+              pathname === `/${category.toLowerCase()}` ||
+              (pathname === "/" && category === "Home Page")
+                ? activeLink
+                : inactiveLink
+            }
+            key={category}
+          >
+            {category}
+          </Link>
+        ))}
       </nav>
     </aside>
   );
